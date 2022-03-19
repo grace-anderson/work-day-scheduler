e4268a538df58b6265c69fa02ddda4ad14b7b318
Grace <helen.g.anderson@me.com>
2022-03-19 19:22:31 +1100

    Completed updating classes for past and future time rows

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,20 @@
 var today = moment();
 $("#currentDay").text(today.format("[Today is] dddd, Do MMMM YYYY"));
 
-//highlight row that matches current hour
 
-//when real time is within the rows current hour
-//update the background of the event table cell to red (class .present)
-//target row id to see if the row hour matches current time hour (e.g. id="2")
-//e.g. when the time is 2:06 pm the background
-//of the 2PM Event table cell should be red
-
+//TO DO - text hour changes between 9 am and 5 pm.
 function highlightHour() {
   var currentHour = moment().hour();
-  console.log(currentHour);
-
-  //check is time row is current hour (class = time-block, id = hour for each row)
-  //use j.query each as is js for loop
-
+//   console.log(currentHour);
   $(".time-block").each(function () {
     var timeRow = parseInt($(this).attr("id"));
-    console.log(timeRow);
+    // console.log(timeRow);
     if (timeRow === currentHour) {
       $(this).addClass("present");
+    } else if (timeRow < currentHour) {
+        $(this).addClass("past");
+    } else {
+        $(this).addClass("future");
     }
   });
 }
