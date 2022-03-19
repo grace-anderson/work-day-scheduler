cef640daccae04db34ae440a00b5849660eb862c
Grace <helen.g.anderson@me.com>
2022-03-19 14:03:46 +1100

    added HTML for hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,11 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, Do MMMM"));
 
-//highlight row that is within current hour when page loaded or refreshed
+//highlight row that matches current hour
+var currentHour = moment();
+
+//when real time is within the time block's hour
+//update the background of that table cell to red (class .present)
+//target table cells have id that matches row time (e.g. id="2pm")
+//e.g. when the time is 2:06 pm the background 
+//of the 2PM Event table cell should be red
