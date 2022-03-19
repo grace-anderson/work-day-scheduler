4f879065575df0c63865594a6966d54d1683b1f7
Grace <helen.g.anderson@me.com>
2022-03-19 15:04:27 +1100

    Completed highlighting current hour's row

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,29 @@
-
 //get today's date
 var today = moment();
-$("#currentDay").text(today.format("dddd, Do MMMM"));
+$("#currentDay").text(today.format("[Today is] dddd, Do MMMM YYYY"));
 
 //highlight row that matches current hour
-var currentHour = moment();
 
-//when real time is within the time block's hour
-//update the background of that table cell to red (class .present)
-//target table cells have id that matches row time (e.g. id="2pm")
-//e.g. when the time is 2:06 pm the background 
+//when real time is within the rows current hour
+//update the background of the event table cell to red (class .present)
+//target row id to see if the row hour matches current time hour (e.g. id="2")
+//e.g. when the time is 2:06 pm the background
 //of the 2PM Event table cell should be red
+
+function highlightHour() {
+  var currentHour = moment().hour();
+  console.log(currentHour);
+
+  //check is time row is current hour (class = time-block, id = hour for each row)
+  //use j.query each as is js for loop
+
+  $(".time-block").each(function () {
+    var timeRow = parseInt($(this).attr("id"));
+    console.log(timeRow);
+    if (timeRow === currentHour) {
+      $(this).addClass("present");
+    }
+  });
+}
+
+highlightHour();
