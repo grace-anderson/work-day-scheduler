2fce5843ba0a3a0f53878d16b39ef6414d3ab481
Grace <helen.g.anderson@me.com>
2022-03-19 13:53:32 +1100

    Added dynamic today's date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-//highlight row within current hour when page loaded or refreshed
+
+//get today's date
+var today = moment();
+$("#currentDay").text(today.format("dddd, Do MMMM"));
+
+//highlight row that is within current hour when page loaded or refreshed
