bf4d8ae903962ece2f80c4aeecba0a925109c6fa
Grace <helen.g.anderson@me.com>
2022-03-19 21:02:16 +1100

    created text area and save button. Updated css styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var today = moment();
 $("#currentDay").text(today.format("[Today is] dddd, Do MMMM YYYY"));
 
 
-//TO DO - test hour changes between 9 am and 5 pm.
+//TO DO - test hour changes automatically between 9 am and 5 pm.
 function highlightHour() {
   var currentHour = moment().hour();
 //   console.log(currentHour);
@@ -22,4 +22,21 @@ function highlightHour() {
 
 highlightHour();
 
-//Update middle column into a text area, for user to enter text
+//HTML - Update middle column into a text area, for user to enter text
+//add text area to middle column
+//add class description (for the textarea)
+//save added text locally
+// TO DO - Should the user enter on return and Save? Or only with Save? Requirement specifies clicking save button only
+//=> create save button in right column
+//add .saveBtn class
+// add save icon to save button 
+
+// TO DO - save text added to text area to local storage when save button clicked
+
+
+
+
+
+//TO DO - put a limit on when text can be added. e.g. can text be added to past rows? (not mentioned in requirements)
+//TO DO - look at updating from a table to bootstrap columns with "mini forms" for each row 
+//
