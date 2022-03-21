7e55c3b3942969fb3869eeec1543dcf37084d308
Grace <helen.g.anderson@me.com>
2022-03-21 23:44:13 +1100

    updated .saveBtn function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,6 @@ $(document).ready(function () {
 
   $("#currentDay").text(today.format("[Today is] dddd, Do MMMM YYYY"));
 
-  //   refresh page
-  //   location.reload(false);
-
   // update colour blocks (past, present, future) when time updates
   function highlightHour() {
     var currentHour = moment().hour();
@@ -28,54 +25,56 @@ $(document).ready(function () {
 
   highlightHour();
 
+  //refresh time, to run highlightHour to check for updating time block colour
   setInterval(highlightHour, 10000);
 
-  //refresh time, to run highlightHour to check for updating colour
+  //prepare alert
+  var alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+
+  function alert(message, type) {
+    var wrapper = document.createElement("div");
+    wrapper.innerHTML =
+      '<div class="alert alert-danger alert-' +
+      type +
+      ' alert-dismissible" role="alert">' +
+      message;
+
+    alertPlaceholder.append(wrapper);
+  }
 
   //button to save text
   $(".saveBtn").on("click", function () {
+    
     var timeSlot = $(this).parent().attr("id");
-
     var textId = "#text-" + timeSlot;
     var textValue = $.trim($(textId).val());
+    console.log("textValue", textValue);
 
-    if (textValue === "") {
-      //prepare alert
-      var alertPlaceholder = document.getElementById("liveAlertPlaceholder");
-
-      function alert(message, type) {
-        var wrapper = document.createElement("div");
-        wrapper.innerHTML =
-          '<div class="alert alert-danger alert-' +
-          type +
-          ' alert-dismissible" role="alert">' +
-          message;
-
-        alertPlaceholder.append(wrapper);
-      }
-      //show alert
-      //issue with alert only showing after page refreshed
-      alert("Enter some text in your event");
-      $(liveAlertPlaceholder).fadeOut(3000);
-
+    if (textValue !== "" || textValue === null) {
       localStorage.setItem(timeSlot, textValue);
       console.log("time and text captured: " + timeSlot + ", " + textValue);
     } else {
+      //show alert
+      //TO DO issue with alert only showing after page refreshed
+      alert("Enter some text in your event");
+      $(liveAlertPlaceholder).fadeOut(3000);
+
+      textValue = "";
 
       localStorage.setItem(timeSlot, textValue);
+
       console.log("time and text captured: " + timeSlot + ", " + textValue);
     }
   });
 
-  //get saved items from local storage
-    $("#text-9").val(localStorage.getItem("9"));
-    $("#text-10").val(localStorage.getItem("10"));
-    $("#text-11").val(localStorage.getItem("11"));
-    $("#text-12").val(localStorage.getItem("12"));
-    $("#text-13").val(localStorage.getItem("13"));
-    $("#text-14").val(localStorage.getItem("14"));
-    $("#text-15").val(localStorage.getItem("15"));
-    $("#text-16").val(localStorage.getItem("16"));
-    $("#text-17").val(localStorage.getItem("17"));
-
+  //display saved items from local storage
+  $("#text-9").val(localStorage.getItem("9"));
+  $("#text-10").val(localStorage.getItem("10"));
+  $("#text-11").val(localStorage.getItem("11"));
+  $("#text-12").val(localStorage.getItem("12"));
+  $("#text-13").val(localStorage.getItem("13"));
+  $("#text-14").val(localStorage.getItem("14"));
+  $("#text-15").val(localStorage.getItem("15"));
+  $("#text-16").val(localStorage.getItem("16"));
+  $("#text-17").val(localStorage.getItem("17"));
 });
